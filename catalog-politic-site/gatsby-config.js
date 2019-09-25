d83270430d828a894ecca222aaa6d260f45d1f8d
Robert Badea <owlree128@gmail.com>
2019-09-25 17:42:47 +0000

    Fix prefixPath Gatsby option

--- catalog-politic-site/gatsby-config.js
+++ catalog-politic-site/gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
+  pathPrefix: '/dev2',
   siteMetadata: {
     title: `Catalog Politic`,
     description: `Catalog Politic este o platformă online care centralizează informațiile existente despre persoanele din funcții publice din România. Scopul este de a pune la dispoziția cetățenilor informații obiective cu privire la cei care le reprezintă interesele. Mai mult, prin baze de date complete și ușor de utilizat, proiectul oferă jurnaliștilor și cercetătorilor suport în activitățile de documentare.`,
     author: `Code for Romania`,
-    pathPrefix: `/dev2`,
     locales: ['ro', 'en'],
     menuLinks: [{
       name: 'Politicians',
