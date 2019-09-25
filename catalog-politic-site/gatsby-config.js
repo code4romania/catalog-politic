457b985931213ce34dad01b609cb19fc9b66a2a2
Robert Badea <owlree128@gmail.com>
2019-09-25 14:57:40 +0000

    Remove message

--- catalog-politic-site/gatsby-config.js
+++ catalog-politic-site/gatsby-config.js
@@ -46,8 +46,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        display: `minimal-ui`
       },
     },
     {
